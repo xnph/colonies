9dcc104d638937b9ae1637ef0b4cf72c4419b168
Bhavish J <bhavishj@live.co.uk>
2014-08-25 12:06:59 +0100

    The main .py (for now)

---
+++ colonies.py
@@ -0,0 +1,15 @@
+#Colonies - Space colony management sim
+#Created by Bhavish J (Github: xnph)
+
+#Learning python and github. Two birds with one stone.
+
+
+
+
+class Planet:
+    """Class representing planets and handles generation and properties of
+planets and such"""
+
+    planettypes = ["desert","ocean","terra"] #will add more types later on
+
+    
